7549f81d290b4e7c177195e9274421c2d36fb819
Rinnegatamante <rinnegatamante@gmail.com>
2018-03-16 21:41:03 +0100

    Added multi-language support (#3)
    * Added a cool homebrew compatibility page linked with GitHub issue tracker.
    
    * Properly formatting code.
    
    * Added multi-language support.
    
    * Typo fix.
    
    * Reduced translation string number.
    
    * Reverting to favicon.ico

---
+++ texts/index.js
@@ -0,0 +1,77 @@
+﻿var texts = []
+
+// English
+texts.push([
+	"About", // 0
+	"Download", // 1
+	"Community", // 2
+	"Compatibility", // 3
+	"Showcase", // 4
+	"Contribute", // 5
+	"Donate", // 6
+	"The world's first PlayStation Vita emulator.", // 7
+	"Find Out More", // 8
+	"About Vita3K", // 9
+	"Vita3K is the world's first experimental open-source PlayStation Vita emulator for Windows and MacOs.", // 10
+	"Please note that the purpose of the emulator is not to enable illegal activity.", // 11
+	"Pick up your build!", // 12
+	"Coming Soon!", // 13
+	"Grab from", // 14
+	"Get in touch with us!", // 15
+	"Get in touch with the developers and other users whenever you want. We prefer Discord server but the choice is up to you!", // 16
+	"Join now!", // 17
+	"Check out what currently runs!", // 18
+	"The emulator does not yet run <b>any commercial games</b>.<br>Several homebrew games are supported. Please check the <a href=\"compatibility.html?lang=en\">compatibility list</a> for further details!", // 19
+	"Homebrew Game Port", // 20
+	"Homebrew Game", // 21
+	"Homebrew Utility", // 22
+	"Help us getting better!", // 23
+	"You can contribute to the emulator in the following ways:", // 24
+	"File issues or make Pull Requests on our <a href=\"https://github.com/Vita3K/Vita3K\">GitHub repository</a>.", // 25
+	"Update the <a href=\"https://github.com/Vita3K/homebrew-compatibility/issues\">homebrew compatibility database</a>.", // 26
+	"Show us your love!", // 27
+	"If you would like to show your appreciation and even help fund development, the project has a <a href=\"https://www.patreon.com/Vita3K\">Patreon Page</a>.", // 28
+	"Become a Patron", // 29
+	"PlayStation and PS Vita are trademarks of Sony Interactive Entertainment Inc.", // 30
+	"The emulator is not related to or endorsed by Sony, or derived from confidential materials belonging to Sony.", // 31
+	"Logo by", // 32
+	"Select Language" // 33
+])
+
+// Italian (translation provided by Rinnegatamante)
+texts.push([
+	"Riguardo", // 0
+	"Download", // 1
+	"Comunità", // 2
+	"Compatibilità", // 3
+	"Galleria", // 4
+	"Contribuisci", // 5
+	"Dona", // 6
+	"Il primo emulatore Playstation Vita del mondo.", // 7
+	"Scopri di più", // 8
+	"Riguardo Vita3K", // 9
+	"Vita3K è il primo emulatore sperimentale PlayStation Vita del mondo per Windows e MacOs.", // 10
+	"Nota bene che lo scopo dell'emulatore non è quello di abilitare attività illecite.", // 11
+	"Scegli la tua piattaforma!", // 12
+	"Prossimamente!", // 13
+	"Scarica da", // 14
+	"Contattaci!", // 15
+	"Mettiti in contatto con i programmatori e con altri utenti quando preferisci. Preferiamo l'utilizzo del server Discord ma la scelta spetta a te!", // 16
+	"Unisciti ora!", // 17
+	"Controlla cosa funziona attualmente!", // 18
+	"L'emulatore attualmente non esegue <b>nessun titolo commerciale</b>.<br>Tuttavia diverse homebrew sono supportate. Controlla la <a href=\"compatibility.html?lang=it\">lista di compatibilità</a> per ulteriori dettagli!", // 19
+	"Port Amatoriale", // 20
+	"Gioco Homebrew", // 21
+	"Tool Homebrew", // 22
+	"Aiutaci a migliorare!", // 23
+	"Puoi contribuire allo sviluppo dell'emulatore in diversi modi:", // 24
+	"Riporta problemi o prepara delle Pull Request sulla nostra <a href=\"https://github.com/Vita3K/Vita3K\">repository GitHub</a>.", // 25
+	"Aggiorna il <a href=\"https://github.com/Vita3K/homebrew-compatibility/issues\">database delle homebrew compatibili</a>.", // 26
+	"Mostraci il tuo amore!", // 27
+	"Se vuoi dimostrarci il tuo apprezzamento e finanziare lo sviluppo, il progetto ha una <a href=\"https://www.patreon.com/Vita3K\">pagina Patreon</a>.", // 28
+	"Diventa un Patron", // 29
+	"PlayStation e PS Vita sono marchi registrati di Sony Interactive Entertainment Inc.", // 30
+	"L'emulatore non è correlato o certificato da Sony, nè derivato da materiale confidenziale appartenente alla Sony.", // 31
+	"Logo creato da", // 32
+	"Seleziona Lingua" // 33
+])
