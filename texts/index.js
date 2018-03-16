efc45e1f3653e92abcf8c22eac45392bcdab5282
VelocityRa <makren67@gmail.com>
2018-03-17 01:05:28 +0200

    Add Greek translation

--- texts/index.js
+++ texts/index.js
@@ -18,14 +18,14 @@ texts.push([
 	"Coming Soon!", // 13
 	"Grab from", // 14
 	"Get in touch with us!", // 15
-	"Get in touch with the developers and other users whenever you want. We prefer Discord server but the choice is up to you!", // 16
+	"Get in touch with the developers and other users whenever you want. We prefer via our Discord server, but the choice is up to you!", // 16
 	"Join now!", // 17
 	"Check out what currently runs!", // 18
 	"The emulator does not yet run <b>any commercial games</b>.<br>Several homebrew games are supported. Please check the <a href=\"compatibility.html?lang=en\">compatibility list</a> for further details!", // 19
 	"Homebrew Game Port", // 20
 	"Homebrew Game", // 21
 	"Homebrew Utility", // 22
-	"Help us getting better!", // 23
+	"Help us get better!", // 23
 	"You can contribute to the emulator in the following ways:", // 24
 	"File issues or make Pull Requests on our <a href=\"https://github.com/Vita3K/Vita3K\">GitHub repository</a>.", // 25
 	"Update the <a href=\"https://github.com/Vita3K/homebrew-compatibility/issues\">homebrew compatibility database</a>.", // 26
@@ -82,13 +82,13 @@ texts.push([
 	"Télécharger", // 1
 	"Communauté", // 2
 	"Compatibilité", // 3
-	"Galerie", // 4 
+	"Galerie", // 4
 	"Contribuer", // 5
 	"Faire un don", // 6
 	"Le premier émulateur au monde pour la PlayStation Vita.", // 7
 	"En savoir plus", // 8
 	"À propos de Vita3K", // 9
-	"Vita3K est le tout premier émulateur open-source pour la PlayStation Vita compatible avec Windows et MacOs.", // 10 
+	"Vita3K est le tout premier émulateur open-source pour la PlayStation Vita compatible avec Windows et MacOs.", // 10
 	"Cet émulateur n'a pas pour but de permettre l'activité illégale.", // 11
 	"Choisissez votre version !", // 12
 	"Bientôt !", // 13
@@ -149,5 +149,43 @@ texts.push([
 	"PlayStation و PS Vita هما علامتين تجاريتين لشركة Sony Interactive Entertainment Inc.", // 30
 	"المحاكي غير مرتبط او مؤيد من ٍSony, او مأخوذ من مستندات سرية تنتمي الى Sony. ", // 31
 	"الشعار من قبل", // 32
-	"اختر اللغة" // 33 
+	"اختر اللغة" // 33
+])
+
+// Greek (translation by VelocityRa)
+texts.push([
+	"Περί", // 0
+	"Κατεβάσε", // 1
+	"Κοινότητα", // 2
+	"Συμβατότητα", // 3
+	"Γκαλερί", // 4
+	"Βοηθήστε", // 5
+	"Δωρεά", // 6
+	"Ο πρώτος εξομοιωτής για το PlayStation Vita στον κόσμο.", // 7
+	"Μάθετε περισσότερα", // 8
+	"Σχετικά με το Vita3K", // 9
+	"Το Vita3K είναι ο πρώτος πειραματικός εξομοιωτής PlayStation Vita ανοικτού κώδικα για Windows και Mac OS", // 10
+	"Σημειώστε ότι ο σκοπός του εξομοιωτή δεν είναι να επιτρέπει την παράνομη δραστηριότητα.", // 11
+	"Πάρτε το build σας!", // 12
+	"Σύντομα!", // 13
+	"Πάρτο απ'το", // 14
+	"Ελάτε σε επαφή μαζί μας!", // 15
+	"Ελάτε σε επαφή με τους προγραμματιστές και τους άλλους χρήστες όποτε θέλετε. Προτιμούμε το Discord αλλά η επιλογή εξαρτάται από εσάς!", // 16
+	"Εγγραφείτε τώρα!", // 17
+	"Δείτε τι τρέχει τώρα!", // 18
+	"Ο εξομοιωτής <b> δεν εκτελεί ακόμα εμπορικά παιχνίδια </b>. <br> Υποστηρίζονται διάφορα παιχνίδια homebrew. Ελέγξτε τη <a href=\"compatibility.html?lang=en\"> λίστα συμβατότητας </ b> a> για περισσότερες λεπτομέρειες! ", // 19
+	"Μετατρεμμένο παιχνίδι Homebrew", // 20
+	"Παιχνίδι Homebrew", // 21
+	"Βοηθητικό πρόγραμμα Homebrew", // 22
+	"Βοηθήστε μας να γίνουμε καλύτεροι!", // 23
+	"Μπορείτε να συνεισφέρετε με τους ακόλουθους τρόπους:", // 24
+	"Απαίτηση αρχείων ή δημιουργία αιτήσεων έλξης στο <a href=\"https://github.com/Vita3K/Vita3K\"> αποθετήριο GitHub </a>.", // 25
+	"Ενημερώστε τη <a href=\"https://github.com/Vita3K/homebrew-compatibility/issues\"> βάση δεδομένων συμβατότητας homebrew </a>.", // 26
+	"Δείξε μας την αγάπη σας!", // 27
+	"Εάν θέλετε να δείξετε την εκτίμησή σας και ακόμη και να βοηθήσετε την ανάπτυξη των πόρων, το έργο έχει <a href=\"https://www.patreon.com/Vita3K\">Σελίδα Patreon</a>.", // 28
+	"Γίνετε Patreon", // 29
+	"Το PlayStation και το PS Vita είναι εμπορικά σήματα της Sony Interactive Entertainment Inc.", // 30
+	"Ο εξομοιωτής δεν σχετίζεται με την Sony ούτε προέρχεται από εμπιστευτικά υλικά που ανήκουν στη Sony.", // 31
+	"Λογότυπο από", // 32
+	"Επιλογή γλώσσας" // 33
 ])
