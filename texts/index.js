5ab2a3316778c9efd54c9dc01b95a868f630b043
Marcilio Junior <marciliojr.farias@gmail.com>
2019-02-05 12:35:32 -0300

    Brazilian fixes (#69)
    * Update index.js
    
    * Brazilian fix

--- texts/index.js
+++ texts/index.js
@@ -329,10 +329,10 @@ texts.push([
 	"Mostruário", // 4
 	"Contribua", // 5
 	"Doação", // 6
-	"O primeiro emulador de PlayStation Vita do mundo.", // 7
+	"O primeiro emulador funcional de PlayStation Vita do mundo.", // 7
 	"Descubra mais", // 8
 	"Sobre Vita3K", // 9
-	"Vita3K é o primeiro emulador experimental código aberto de PlayStation Vita no mundo para Windows e macOS.", // 10
+	"Vita3K é o primeiro emulador funcional experimental código aberto de PlayStation Vita no mundo para Windows e macOS.", // 10
 	"Tenha em mente que o propósito do emulador não é permitir atividades ilegais.", // 11
 	"Pegue sua versão!", // 12
 	"Em breve!", // 13
@@ -357,7 +357,7 @@ texts.push([
 	"Logo por", // 32
 	"Selecione o Idioma", // 33
 	"O que há de novo.", // 34
-	"Nós temos um blog onde nós postamos mensalmente um detalhado relatorio dos nossos ultimos progressos no nosso trabalho. Venha ler o que há de novo!" // 35
+	"Nós temos um blog onde nós postamos relatorios detalhados dos nossos ultimos progressos no nosso trabalho. Venha ler o que há de novo!" // 35
 ])
 
 // Russian (Translation provided by DumF0rGaming)
